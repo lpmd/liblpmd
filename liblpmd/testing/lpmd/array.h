46da89877ec202853eb815b735e9cbc16ff2d306
sdavis <sdavis@11e9678f-4169-4518-a5ff-2ecc74d24d21>
2009-05-05 11:02:55 +0000

    - Nueva clase template lpmd::Array para reemplazar std::vector en el API - Directorio benchmarks/. tiene un test de Array versus std::vector - Idea de la organizacion de directorios:   * Unit tests van en tests/.  (con LPUnit)   * Tests de integracion van en demos/.   * Tests de eficiencia van en benchmarks/.

---
+++ liblpmd/testing/lpmd/array.h
@@ -0,0 +1,59 @@
+/*
+ *
+ *
+ *
+ */
+
+#ifndef __LPMD_ARRAY_H__
+#define __LPMD_ARRAY_H__
+
+#include <vector>
+#include <algorithm>
+
+namespace lpmd
+{
+
+template <typename T> class Array: private std::vector<T>
+{
+ public:
+  Array() { }
+
+  Array(long int is): std::vector<T>(is) { }
+
+  Array(const T * buffer, long int s): std::vector<T>(s)
+  {
+   for (long int i=0;i<s;++i) (static_cast<std::vector<T> &>(*this))[i] = buffer[i];
+  }
+
+  inline T & operator[](long int i) { return (static_cast<std::vector<T> &>(*this)).operator[](i); }
+
+  inline const T & operator[](long int i) const { return (static_cast<const std::vector<T> &>(*this)).operator[](i); }
+
+  inline long int Size() const { return static_cast<const std::vector<T> &>(*this).size(); }
+
+  inline void Append(const T & x) { push_back(x); }
+
+  inline void Clear() { (static_cast<std::vector<T> &>(*this)).clear(); }
+
+  inline void Delete(long int i) 
+  { 
+   typename std::vector<T>::iterator it=(static_cast<std::vector<T> &>(*this).begin()+i);
+   static_cast<std::vector<T> &>(*this).erase(it);
+  }
+
+  inline long int Find(const T & t)
+  {
+   typename std::vector<T>::iterator b=(static_cast<std::vector<T> &>(*this).begin());
+   typename std::vector<T>::iterator e=(static_cast<std::vector<T> &>(*this).end());
+   typename std::vector<T>::iterator it=std::find(b, e, t);
+   if (it != e) return (it-b);
+   else return -1;
+  }
+
+ private:
+};
+
+} // lpmd
+
+#endif
+
