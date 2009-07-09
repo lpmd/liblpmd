f9b62d4c05794d7250ebb84553d4c82716965432
jperalta <jperalta@11e9678f-4169-4518-a5ff-2ecc74d24d21>
2009-07-09 21:30:51 +0000

    Soporte en openmp para pairpotential

--- liblpmd/testing/pairpotential.cc
+++ liblpmd/testing/pairpotential.cc
@@ -22,22 +22,30 @@ void PairPotential::UpdateForces(Configuration & conf)
  BasicParticleSet & atoms = conf.Atoms();
  const long int n = atoms.Size();
  energycache = 0.0;
- double tmpvir = 0.0,etmp=0.0e0;
+ double tmpvir = 0.0e0,etmp=0.0e0;
  double stress[3][3];
- for (int i=0;i<3;i++)
+ for (int i=0;i<3;++i)
  {
-  for (int j=0;j<3;j++) stress[i][j]=0.0e0;
+  for (int j=0;j<3;++j) stress[i][j]=0.0e0;
  }
- long i,k;
+ long i,k,l;
+ AtomPair nn;
 #ifdef _OPENMP
-#pragma omp parallel for private ( i,k,ff ) reduction ( + : etmp, tmpvir )
+#pragma omp parallel \
+ private ( i,nn,k,l ) \
+ reduction ( + : etmp, tmpvir )
+#endif
+
+#ifdef _OPENMP
+#pragma omp for
 #endif
  for (i=0;i<n;++i)
  {
-  NeighborList & nlist = conf.Neighbors(i, false, GetCutoff());
+  NeighborList nlist;
+  conf.GetCellManager().BuildNeighborList(conf, i, nlist, false, GetCutoff());
   for (k=0;k<nlist.Size();++k)
   {
-   const AtomPair & nn = nlist[k];
+   nn = nlist[k];
    if (AppliesTo(atoms[i].Z(), nn.j->Z()) && nn.r < GetCutoff()) 
    {
     etmp += pairEnergy(nn.r);
@@ -45,11 +53,11 @@ void PairPotential::UpdateForces(Configuration & conf)
     atoms[i].Acceleration() += ff*(forcefactor/atoms[i].Mass());
     nn.j->Acceleration() -= ff*(forcefactor/nn.j->Mass());
     tmpvir -= Dot(nn.rij, ff);
-    for (int k=0;k<3;k++)
+    for (l=0;l<3;++l)
     {
-     stress[0][k] += -(nn.rij)[0]*ff[k];
-     stress[1][k] += -(nn.rij)[1]*ff[k];
-     stress[2][k] += -(nn.rij)[2]*ff[k];
+     stress[0][l] += -(nn.rij)[0]*ff[l];
+     stress[1][l] += -(nn.rij)[1]*ff[l];
+     stress[2][l] += -(nn.rij)[2]*ff[l];
     }
    }
   }
