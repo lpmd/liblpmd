c9701ac4fa7327df23f118c7b59841c4ba6091c9
sdavis <sdavis@11e9678f-4169-4518-a5ff-2ecc74d24d21>
2009-11-12 17:48:17 +0000

    - Nueva escala de color en ColorFromScalar

--- liblpmd/testing/color.cc
+++ liblpmd/testing/color.cc
@@ -6,6 +6,7 @@
 
 using namespace lpmd;
 
+/* 
 Color lpmd::ColorFromScalar(const double & x)
 {
  double R,G,B,A=0.5;
@@ -15,3 +16,15 @@ Color lpmd::ColorFromScalar(const double & x)
  Color clr(R,G,B);
  return clr;
 }
+*/
+
+Color lpmd::ColorFromScalar(const double & x)
+{
+ if (x < 0.0) return Color(0.0, 0.0, 1.0);
+ else if ((x >= 0.0) && (x < 0.25)) return Color(0.0, 4.0*x, 1.0);
+ else if ((x >= 0.25) && (x < 0.5)) return Color(0.0, 1.0, 1.0-4.0*(x-0.25));
+ else if ((x >= 0.5) && (x < 0.75)) return Color(4.0*(x-0.5), 1.0, 0.0);
+ else if ((x >= 0.75) && (x <= 1.0)) return Color(1.0, 1.0-4.0*(x-0.75), 0.0);
+ return Color(1.0, 0.0, 0.0);
+}
+
