33804cbd101163babb6e05d776a42edb4fae0747
sdavis <sdavis@11e9678f-4169-4518-a5ff-2ecc74d24d21>
2009-12-14 12:34:14 +0000

    - Potenciales soportan asignarles especies (-1, -1) para indicar que aplican a todos los atomos   (por ej. constantforce)

--- liblpmd/testing/potential.cc
+++ liblpmd/testing/potential.cc
@@ -16,7 +16,8 @@ Potential::~Potential() { }
 
 void Potential::Initialize(Configuration & conf) { assert(&conf != 0); energycache = 0; }//icc 869
 
+// s1 == s2 == -1 is used as a wildcard
 void Potential::SetValidSpecies(int s1, int s2) { spc_sum = s1+s2; }
 
-bool Potential::AppliesTo(int s1, int s2) const { return ((s1+s2) == spc_sum); }
+bool Potential::AppliesTo(int s1, int s2) const { return ((spc_sum < 0) || ((s1+s2) == spc_sum)); }
 
