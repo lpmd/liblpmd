8b05cbd8ca8a9bb3ca1c46c03fb20ddadf7b1383
fgonzalez <fgonzalez@11e9678f-4169-4518-a5ff-2ecc74d24d21>
2010-01-17 06:05:24 +0000

    Error en el comentario de forcefactor en session.cc: Convierte de eV/A  a  amu*A/fs^2

--- liblpmd/testing/session.cc
+++ liblpmd/testing/session.cc
@@ -21,7 +21,7 @@ Session::Session(): Module("session")
  
  AssignParameter("libraryversion", VERSION);
 
- AssignParameter("forcefactor", "0.0096485341");  // Conversion: 1 (eV/(angstrom*amu)) = FORCEFACTOR (angstrom/(fs^2))
+ AssignParameter("forcefactor", "0.0096485341");  // Conversion: 1 (eV/angstrom) = FORCEFACTOR (amu*angstrom/(fs^2))
  AssignParameter("kboltzmann", "8.6173422E-05");  // k_B in (eV/K)
  AssignParameter("kin2ev", "103.64269");          // Conversion: 1 (amu*(angstrom/fs)^2) = KIN2EV (eV)
  AssignParameter("pressfactor", "160217.65");     // Conversion: 1 (ev/(angstrom^3)) = PRESSFACTOR (MPa)
